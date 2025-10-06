c01ce78c613b1109d340748a4d293ec095bbbe77
Gustavolopes2303 <gls23032007@gmail.com>
2025-10-06 15:24:40 -0300

    teste1.py

--- teste1.py
+++ teste1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.write("Hello world")
 st.title("Saudação")
-nome=st.texto_input("Digite seu nome")
+nome = st.texto_input("Digite seu nome")
 if nome:
-st.write(nome.upper())
+   st.write(nome.upper())
  
