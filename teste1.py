a649bb3fd51dca15ca1530bbe82a840084b9789f
Gustavolopes2303 <gls23032007@gmail.com>
2025-10-12 13:22:49 -0300

    teste1.py

--- teste1.py
+++ teste1.py
@@ -1,10 +1,4 @@
-import streamlit as st
-st.write("Hello world")
-st.title("Saudação")
-nome = st.text_input("Digite seu nome")
-if nome: 
-   st.write(nome.upper())
-import streamlit as st
+
 import random
 
 # --- Configurações da Página ---
