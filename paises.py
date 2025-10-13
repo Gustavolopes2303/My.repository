b7f2f0e7bb4ca7f4c01a2e7f8532c70a3682bf98
Gustavolopes2303 <gls23032007@gmail.com>
2025-10-13 15:11:46 -0300

    paises.py

--- paises.py
+++ paises.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import streamlit as st
+import plotly.express as px
 
 dataset= pd.read_csv('https://www.irdx.com.br/media/uploads/paises.csv')
 
-st.dataframe(dataset)
+fig = px.scatter_geo(dataset,
+                     lat=dataset['latitude'],
+                     lon=dataset['longitude'],
+                     hover_name=dataset['nome'])
+fig.update_layout(title='Coordenadas paises do Mundo',
+                  geo_scope='world')
+st.ploatly_chart(fig. use_container_width=True, theme="streamlit")
